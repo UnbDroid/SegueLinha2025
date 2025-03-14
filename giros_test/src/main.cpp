dfb93b52da7d50bebd1f4fef0e53ab2fe8d184f7
Pedro <phm.pedroalves@gmail.com>
2025-03-14 13:57:25 -0300

    subindo código sensor

--- giros_test/src/main.cpp
+++ giros_test/src/main.cpp
@@ -1,80 +1,44 @@
 #include <Arduino.h>
-#include <MPU9250.h>
 
-// Defina os pinos que você vai usar
-const int pinSDA = 21; // Pino SDA
-const int pinSCL = 22; // Pino SCL
+const int pinLDR[7] = {A0, A1, A2, A3, A4, A5, A6};
 
-// Configurar o objeto MPU9250 com os pinos definidos
-MPU9250 IMU(Wire, 0x68);
-int status;
+const int threshold = 500; // Ajuste conforme necessário
 
-// Variáveis para armazenar os ângulos integrados
-float angleX = 0;
-float angleY = 0;
-float angleZ = 0;
+void setup()
+{
 
-// Variável para armazenar o tempo anterior
-unsigned long prevTime = 0;
-
-void setup() {
-  // Configurar os pinos I2C
-  Wire.begin(pinSDA, pinSCL);
-
-  // Serial para exibir dados
   Serial.begin(9600);
-  while (!Serial) {}
-
-  Serial.println("Iniciando...");
-
-  // Iniciar comunicação com o IMU
-  status = IMU.begin();
-  if (status < 0) {
-    Serial.println("IMU initialization unsuccessful");
-    Serial.println("Check IMU wiring or try cycling power");
-    Serial.print("Status: ");
-    Serial.println(status);
-    while (1) {}
-  } else {
-    Serial.println("IMU initialized successfully");
+  while (!Serial)
+  {
   }
 
-  // Inicializar o tempo anterior
-  prevTime = millis();
+  Serial.println("Iniciando sensores de luz...");
 }
 
-void loop() {
-  // Ler o sensor
-  IMU.readSensor();
+void loop()
+{
 
-  // Calcular o deltaTime
-  unsigned long currentTime = millis();
-  float deltaTime = (currentTime - prevTime) / 1000.0; // Converter para segundos
-  prevTime = currentTime;
+  String output = "";
 
-  // Integrar os valores do giroscópio para calcular os ângulos
-  angleX += IMU.getGyroX_rads() * deltaTime;
-  angleY += IMU.getGyroY_rads() * deltaTime;
-  // Condição para integrar o valor absoluto de GyroZ
-  //angleZ += IMU.getGyroZ_rads() * deltaTime;
+  for (int i = 0; i < 7; i++)
+  {
+    int lightValue = analogRead(pinLDR[i]);
 
-  if (abs(IMU.getGyroZ_rads() * deltaTime) > 0.00005) {
-    angleZ += IMU.getGyroZ_rads() * deltaTime;
+    if (lightValue < threshold)
+    {
+      output += "P";
+    }
+    else
+    {
+      output += "B";
+    }
+
+    if (i < 6)
+    {
+      output += ",";
+    }
   }
-  
 
-  // Converter os ângulos de radianos para graus
-  float angleX_deg = angleX * 180.0 / PI;
-  float angleY_deg = angleY * 180.0 / PI;
-  float angleZ_deg = angleZ * 180.0 / PI;
-
-  // Exibir os ângulos integrados em graus
-  Serial.print("AngleX: ");
-  Serial.print(angleX_deg, 6);
-  Serial.print("\tAngleY: ");
-  Serial.print(angleY_deg, 6);
-  Serial.print("\tAngleZ: ");
-  Serial.println(angleZ_deg, 6);
+  Serial.println(output);
 
   delay(100);
-}
