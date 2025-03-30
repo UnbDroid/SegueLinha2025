fa3dacfa0ae7c711e7fb76cb59d80d958a061a84
mariosantos-05 <marioaugustov.s@gmail.com>
2025-03-29 22:38:07 -0300

    Mock code for test of sensors

--- Test_motor/src/main.cpp
+++ Test_motor/src/main.cpp
@@ -7,6 +7,10 @@
 MotorDC MotorE(ENCA1, ENCB1, ENA, IN2, IN1);
 MotorDC MotorD(ENCA2, ENCB2, ENB, IN3, IN4);
 
+
+// Criação dos objetos QTRSensors
+QTRSensors qtr;
+
 unsigned long pos1 = 0; // Variável para armazenar a posição do encoder do motor 1
 unsigned long pos2 = 0; // Variável para armazenar a posição do encoder do motor 2
 
@@ -14,8 +18,11 @@ unsigned long pos2 = 0; // Variável para armazenar a posição do encoder do mo
 void readencoder1();
 void readencoder2();
 
+const uint8_t SensorCount = 8;
+uint16_t sensorValues[SensorCount];
 
 void setup() {
+
     Serial.begin(9600);
     attachInterrupt(digitalPinToInterrupt(ENCA1), readencoder1, RISING);
     attachInterrupt(digitalPinToInterrupt(ENCA2), readencoder2, RISING);
@@ -23,20 +30,36 @@ void setup() {
     MotorD.configurar(2100, 1.8, 1.3, 0);
     MotorE.configurar(2100, 1.8, 1.3, 0);
 
+    qtr.setTypeRC();
+    qtr.setSensorPins((const uint8_t[]){32, 33, 25, 26, 27, 14, 12, 13}, SensorCount);
+
+    Serial.println("Calibrating...");
+    for (int i = 0; i < 400; i++) {
+        qtr.calibrate();
+        delay(5);
+    }
+    Serial.println("Done calibrating.");
+
 }
 
-void loop() {
-    Serial.println(pos1);
-    Serial.println(pos2);
-    MotorD.ligar_motor(1,200);
-    MotorE.ligar_motor(1,200);
-    delay(1000);
 
-    MotorD.parar();
-    MotorE.parar();
-    delay(1000);
+void loop()
+{
+  // read calibrated sensor values and obtain a measure of the line position
+  // from 0 to 5000 (for a white line, use readLineWhite() instead)
+  uint16_t position = qtr.readLineBlack(sensorValues);
 
+  // print the sensor values as numbers from 0 to 1000, where 0 means maximum
+  // reflectance and 1000 means minimum reflectance, followed by the line
+  // position
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
+  }
+  Serial.println(position);
 
+  delay(250);
 }
 
 void readencoder1() {
