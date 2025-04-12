83607492f6b09ba8d95619faf3ff3ecafab6cca6
Quilherpreto <231011391@aluno.unb.br>
2025-04-12 12:49:15 -0300

    Adicionei o PID dos sensores e mudei o PWM pelo valor regulado

--- Test_motor/src/main.cpp
+++ Test_motor/src/main.cpp
@@ -10,56 +10,15 @@ MotorDC MotorD(ENCA2, ENCB2, IN3, IN4);
 
 // Criação dos objetos QTRSensors
 QTRSensors qtr;
-
-/*unsigned long pos1 = 0; // Variável para armazenar a posição do encoder do motor 1
-unsigned long pos2 = 0; // Variável para armazenar a posição do encoder do motor 2
-
-
-
-const uint8_t SensorCount = 8;
-uint16_t sensorValues[SensorCount];
-#define encoderA 3  // Yellow wire#define encoderA 2  // Yellow wire
-#define encoderB 2  // Green wire
-
-volatile int position = 0; // Keeps track of the encoder position
-int lastAState;
-
-// Interrupt Service Routine (ISR) to read encoder changes
-void readEncoder() {
-  int A = digitalRead(encoderA);
-  int B = digitalRead(encoderB);
-  
-  if (A != lastAState) { // If state of A has changed
-    if (B != A) { 
-      position++; // Clockwise
-    } else {
-      position--; // Counterclockwise
-    }
-  }
-  lastAState = A;
-}
-void setup() {
-  pinMode(encoderA, INPUT_PULLUP);
-  pinMode(encoderB, INPUT_PULLUP);
-  
-  // Attach an interrupt to encoderA (pin 2), triggering on state change
-  attachInterrupt(digitalPinToInterrupt(encoderA), readEncoder, CHANGE);
-
-  Serial.begin(9600);
-  lastAState = digitalRead(encoderA);
-}
-
-void loop() {
-  Serial.println(position); // Print encoder position
-  MotorD.ligar_motor(1,100);
-  MotorE.ligar_motor(-1,255);
-  delay(100);
-
-
-}
-
-*/
-
+int error = 0;
+int m1Speed = 0;
+int m2Speed = 0;
+int motorSpeed = 0;
+int lastError = 0;
+float KP = 0;
+float KD = 0;
+int M1 = 50; //base motor speeds
+int M2 = 50; //base motor speeds
 
 const uint8_t SensorCount = 8;
 uint16_t sensorValues[SensorCount];
@@ -116,6 +75,17 @@ void loop()
   // from 0 to 5000 (for a white line, use readLineWhite() instead)
   uint16_t position = qtr.readLineBlack(sensorValues);
 
+  // compute our "error" from the line position
+  // error is zero when the middle sensor is over the line,
+  error = position - 3500; /// using an 8 sensor array targeting middle of position 4 to 5
+
+  // set the motor speed based on proportional and derivative PID terms
+  motorSpeed = KP * error + KD * (error - lastError);
+  lastError = error;
+  /////////CHANGE THE - OR + BELOW DEPENDING ON HOW YOUR DRIVER WORKS
+  m1Speed = M1 - motorSpeed; // M1 and M2 are base motor speeds.
+  m2Speed = M2 + motorSpeed;
+
   // print the sensor values as numbers from 0 to 1000, where 0 means maximum
   // reflectance and 1000 means minimum reflectance, followed by the line
   // position
@@ -127,17 +97,117 @@ void loop()
   Serial.println(position);
 
   if (sensorValues[1] > 800 && sensorValues[2] > 800 && sensorValues[3] > 800 && sensorValues[4] > 800 && sensorValues[5] > 800 && sensorValues[6] > 800 && sensorValues[7] > 800){
-    MotorD.ligar_motor(1,100);
-    MotorE.ligar_motor(1,100);
+    MotorD.ligar_motor(1,m1Speed);
+    MotorE.ligar_motor(1,m2Speed);
   }
   else if(sensorValues[0] < 500){
-    MotorD.ligar_motor(-1,100);
-    MotorE.ligar_motor(1,100);
+    MotorD.ligar_motor(-1,m1Speed);
+    MotorE.ligar_motor(1,m2Speed);
   }
   else if(sensorValues[7] < 500){
-    MotorD.ligar_motor(1,100);
-    MotorE.ligar_motor(-1,100);
+    MotorD.ligar_motor(1,m1Speed);
+    MotorE.ligar_motor(-1,m2Speed);
   }
 
   delay(250);
 }
+
+/*void forward()
+{
+  TimeCheck();
+  motorDrive(motor1, turnCW, m1Speed);
+  motorDrive(motor2, turnCW, m2Speed);
+}
+
+/* //Apply Brakes, then into Standby
+  motorBrake(motor1);
+  motorBrake(motor2);
+  motorsStandby(); 
+
+void TimeCheck() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis > BlinkTime) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    BlinkIt();
+  }
+}
+
+void motorDrive(boolean motorNumber, boolean motorDirection, int motorSpeed)
+{
+  /*
+    This Drives a specified motor, in a specific direction, at a specified speed:
+    - motorNumber: motor1 or motor2 ---> Motor 1 or Motor 2
+    - motorDirection: turnCW or turnCCW ---> clockwise or counter-clockwise
+    - motorSpeed: 0 to 255 ---> 0 = stop / 255 = fast
+  
+
+  boolean pinIn1; //Relates to AIN1 or BIN1 (depending on the motor number specified)
+
+  //Specify the Direction to turn the motor
+  //Clockwise: AIN1/BIN1 = HIGH and AIN2/BIN2 = LOW
+  //Counter-Clockwise: AIN1/BIN1 = LOW and AIN2/BIN2 = HIGH
+  if (motorDirection == turnCW)
+    pinIn1 = HIGH;
+  else
+    pinIn1 = LOW;
+
+  //Select the motor to turn, and set the direction and the speed
+  if (motorNumber == motor1)
+  {
+    digitalWrite(pinAIN1, pinIn1);
+    digitalWrite(pinAIN2, !pinIn1); //This is the opposite of the AIN1
+    analogWrite(pinPWMA, motorSpeed);
+  }
+  else
+  {
+    digitalWrite(pinBIN1, pinIn1);
+    digitalWrite(pinBIN2, !pinIn1); //This is the opposite of the BIN1
+    analogWrite(pinPWMB, motorSpeed);
+  }
+
+  //Finally , make sure STBY is disabled - pull it HIGH
+  digitalWrite(pinSTBY, HIGH);
+}
+
+void motorBrake(boolean motorNumber)
+{
+  /*
+    This "Short Brake"s the specified motor, by setting speed to zero
+  
+
+  if (motorNumber == motor1)
+    analogWrite(pinPWMA, 0);
+  else
+    analogWrite(pinPWMB, 0);
+}
+
+void motorStop(boolean motorNumber)
+{
+  /*
+    This stops the specified motor by setting both IN pins to LOW
+  
+  if (motorNumber == motor1) {
+    digitalWrite(pinAIN1, LOW);
+    digitalWrite(pinAIN2, LOW);
+  }
+  else
+  {
+    digitalWrite(pinBIN1, LOW);
+    digitalWrite(pinBIN2, LOW);
+  }
+}
+
+void BlinkIt() {
+  if (BlinkCycle == 1) {
+    //Led On
+    digitalWrite(IndicatorLED, HIGH);
+  }
+  if (BlinkCycle == 2) {
+    //Led Off
+    digitalWrite(IndicatorLED, LOW);
+    BlinkCycle = 0; // reset BlinkCycle
+  }
+  BlinkCycle++; // Set BlinkCycle to second mode
+}*/
