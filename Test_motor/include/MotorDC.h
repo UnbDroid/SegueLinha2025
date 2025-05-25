ba33fee73f843d54619ffc39601c784dc0b0c722
mariosantos-05 <marioaugustov.s@gmail.com>
2025-05-25 14:57:29 -0300

    Refact: MotorDC

--- Test_motor/include/MotorDC.h
+++ Test_motor/include/MotorDC.h
@@ -15,13 +15,15 @@ class MotorDC{
         void ligar_motor(int direcao, int pwmVal);
         void ler_encoder();
         void resetar_encoder();
-        void andar_reto(int velocidade_rpm);
+        void set_RPM(int velocidade_rpm);
         volatile double posi; // posição do motor em ticks do encoder
         double rps = 0; // velocidade ATUAL do motor em rotações por segundo
-        int encoder_volta; // valor de encoder referente a uma volta completa da roda
-        double comprimento_roda = 2 * M_PI * 6.272; //TODO: medir o raio da roda real
+        double get_rpm() { return (rps*60); } // função para obter a velocidade do motor em rpm
 
-    private:
+
+        private:
+        int encoder_volta; // valor de encoder referente a uma volta completa da roda
+        double comprimento_roda = 2 * M_PI * 0.016; 
         int ENCA; // Cabo amarelo
         int ENCB; // Cabo branco
         int PWM;
@@ -37,7 +39,7 @@ class MotorDC{
         float eprev = 0;
         float eintegral = 0; // erro acumulado pro cálculo do ki
         int dir = 1; // 1 para frente, -1 para trás (pelo menos essa é a ideia)
-        
+
 };
 
 #endif
--- Test_motor/include/Pins.h
+++ Test_motor/include/Pins.h
@@ -6,20 +6,20 @@
 
 // Pinos dos motores
 
-#define ENCA1 2
-#define ENCB1 4
-#define IN2 13
-#define IN1 15 
+#define ENCA1 15
+#define ENCB1 2
+#define IN2 26
+#define IN1 25 
 
-#define ENCA2 5
+#define ENCA2 4
 #define ENCB2 18
 #define IN4  14 
 #define IN3  12 
 
-#define LEDON 33
+//#define LEDON 33
 
-#define D1 26
-#define D2 25
+#define D1 33
+#define D2 32
 #define D3 16
 #define D4 17
 #define D5 19
